3ccac41d0ed0128642e60a1192c451206c349a89
ssa-a <75652876+ssa-a@users.noreply.github.com>
2023-06-08 15:47:38 +0800

    Update ConfigCenterMain3344

--- cloud-config-center-3344/src/main/java/com/swpu/springcloud/ConfigCenterMain3344.java
+++ cloud-config-center-3344/src/main/java/com/swpu/springcloud/ConfigCenterMain3344.java
@@ -9,5 +9,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigCenterMain3344 {
     public static void main(String[] args) {
         SpringApplication.run(ConfigCenterMain3344.class,args);
+        System.out.println("测试");
     }
 }
